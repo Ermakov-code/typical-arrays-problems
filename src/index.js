6375e414107575f618a1183ef54f584f5ba4a1bd
Ermakov-code <medison2033@gmail.com>
2020-03-15 02:37:29 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }
+  let min = array[0];
+  array.forEach(function(items) {
+    if(min > items){
+      min = items;
+    }
+  })
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }
+  let max = array[1];
+  array.forEach(function(items) {
+    if(max < items){
+      max = items;
+    }
+  })
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }
+  let res = 0;
+  array.forEach(function(items) {
+    res += items;
+  })
+  return res/array.length;
 }
